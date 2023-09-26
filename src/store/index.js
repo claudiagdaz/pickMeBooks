fdb6e3f8c47695d6b99f0239719d227b8749bc01
Claudia Davila <claudiagdaz@gmail.com>
2023-09-26 14:51:06 -0600

    Remove unnecessary svg

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { changeAuthor, changeTitle, formReducer } from './slices/formSlice'
+import { changeAuthor, changeTitle, formReducer } from './slices/formSlice';
 import { addBook, bookListReducer } from './slices/bookListSlice';
 
 const store = configureStore({
